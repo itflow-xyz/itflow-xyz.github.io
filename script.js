c55c6e47478b521f386fc09c52ab6dade4b95a0d
Filippo Zarba <filippozarba00@gmail.com>
2022-09-20 10:37:12 +0200

    remove color at scroll navbar

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// scroll navbar color
-const header = document.querySelector("header");
-
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= 50) {
-    header.classList.add("active_color");
-  } else {
-    header.classList.remove("active_color");
-  }
-});
-
 //open popup
 document.querySelector(".circle").addEventListener("click", () => {
   document.querySelector(".popup").classList.add("active");
