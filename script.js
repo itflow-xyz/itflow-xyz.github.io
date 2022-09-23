7acc369493dc046be608c8a90276db8528f2bc94
Filippo Zarba <filippozarba00@gmail.com>
2022-09-23 16:37:17 +0200

    change circle

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 //open popup
 document.querySelector(".circle").addEventListener("click", () => {
-  document.querySelector(".popup").classList.add("active");
+  document.querySelector(".circle").classList.remove("deactive");
+  document.querySelector(".circle p").classList.remove("deactive");
+  document.querySelector(".popup_content").classList.remove("deactive");
+  document.querySelector(".circle").classList.add("active");
+  document.querySelector(".circle p").classList.add("active");
+  document.querySelector(".popup_content").classList.add("active");
+  document.querySelector("body").classList.add("no-scroll");
 });
 document
-  .querySelector(".popup .popup_close_btn")
+  .querySelector(".popup_content .popup_close_btn")
   .addEventListener("click", () => {
-    document.querySelector(".popup").classList.remove("active");
+    document.querySelector(".circle").classList.remove("active");
+    document.querySelector(".circle p").classList.remove("active");
+    document.querySelector(".popup_content").classList.remove("active");
+    document.querySelector(".circle").classList.add("deactive");
+    document.querySelector(".circle p").classList.add("deactive");
+    document.querySelector(".popup_content").classList.add("deactive");
+    document.querySelector("body").classList.remove("no-scroll");
   });
