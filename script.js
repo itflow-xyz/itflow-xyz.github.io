951e1e2099631e2dfe8c8d1948294a097e303dcb
giuliobosco <giuliobva@gmail.com>
2022-10-19 14:29:21 +0200

    script.js

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const initGoogleAnalytics = async () => {
 }
 
 const initClarity = async () => {
-  console.log("c")
+  console.log("c");
   (function(c,l,a,r,i,t,y){
     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
